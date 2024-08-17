84f8b9fd3d862b49d3655224647ccb330e9d6703
judinus10 <judes.20222403@iit.ac.lk>
2024-08-17 11:54:14 +0530

    add some comments

--- GMD.py
+++ GMD.py
@@ -3,15 +3,20 @@ from bs4 import BeautifulSoup
 import os
 from urllib.parse import urljoin
 
+# Example URL (You can replace this with the website by choice)
 url = "https://unsplash.com/s/photos/nature"
 
+# Sending a request to the website
 r = requests.get(url)
 soup = BeautifulSoup(r.text, 'html.parser')
 
+# Finding all image tags
 images = soup.findAll('img')
 
+# Directory to save images
 os.makedirs('downloaded_images', exist_ok=True)
 
+# Looping through all found images
 for image in images:
     try:
         # Ensure 'alt' and 'src' are in the image tag attributes
