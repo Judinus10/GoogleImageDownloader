2f0357cb44bc7481224bc2dda1f5d7c4f1c9cbe4
judinus10 <judes.20222403@iit.ac.lk>
2024-08-17 11:41:05 +0530

    Extract and Prepare Image Name and URL

--- GMD.py
+++ GMD.py
@@ -13,4 +13,10 @@ images = soup.findAll('img')
 os.makedirs('downloaded_images', exist_ok=True)
 
 for image in images:
-    
+        # Ensure 'alt' and 'src' are in the image tag attributes
+        if 'alt' in image.attrs and 'src' in image.attrs:
+            name = image['alt'].replace(" ", "_")  # Replacing spaces with underscores for filename
+            link = image['src']
+
+            # Convert relative URL to absolute if necessary
+            link = urljoin(url, link)
